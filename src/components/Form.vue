14753d96dbec7298420c9f1f530bd5c0036b1f18
folkaras <efolkaras@gmail.com>
2019-12-03 14:26:15 +0300

    add form

---
+++ src/components/Form.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form">
+    <div class="form__input-wrapper">
+      <input class="form__input" @focus="focused" v-model="note.head" :placeholder="note.headPlaceholder" type="text" />
+      <span class="form__error-message" v-if="error">{{ error }}</span>
+    </div>
+		<div class="form__input-wrapper">
+    <textarea class="form__input form__input--text-area" v-model="note.body" :placeholder="note.bodyPlaceholder" type="text"></textarea>
+		</div>
+    <button type="submit" class="form__submit" @click="addNote">add</button>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: "Form",
+  props: {
+    note: {
+      type: Object,
+      required: true
+    },
+    error: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    focused() {
+      this.$emit("clearError");
+    },
+    addNote() {
+      this.$emit("addNote", this.note);
+    }
+  }
+};
+</script>
